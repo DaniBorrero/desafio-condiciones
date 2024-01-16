79a07aa96a1cab2338b8bbaf065919c689f2886b
DanielBorrero <borrerodaniel89@gmail.com>
2024-01-16 23:29:44 +0100

    start N 2

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,7 +2,6 @@ function borderAdd() {
     let imagen = document.querySelector('#dogImg')
     if (imagen.style.border === 'none') {
         imagen.style.border = '2px solid red'
-        console.log(imagen.style.border)
     } else {
         imagen.style.border = 'none'
     }
