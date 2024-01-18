002b8e902d7db919eaa4048981048f7ac155799c
DanielBorrero <borrerodaniel89@gmail.com>
2024-01-18 22:25:00 +0100

    tercer ejercicio

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,9 +13,26 @@ function verificar() {
     let count2 = Number(document.querySelector('#input-2').value);
     let count3 = Number(document.querySelector('#input-3').value);
     let total = count1+count2+count3;
-    if (total >= 10) {
+    if (total > 10) {
         document.querySelector('#parrafo').innerHTML = 'Llevas demasiados stickers'
     } else {
         document.querySelector('#parrafo').innerHTML = 'Llevas un total de: '+total
+    }
+}
+
+function verificarPass() {
+    let opction1 = document.querySelector('#select1').value;
+    let opction2 = document.querySelector('#select2').value;
+    let opction3 = document.querySelector('#select3').value;
+    let pass = opction1+opction2+opction3;
+
+    console.log(pass)
+
+    if (pass === '911') {
+        alert('password correcto')
+    } else if (pass === '714') {
+        alert('test')
+    } else {
+        alert('hola mundo')
     }
 }
